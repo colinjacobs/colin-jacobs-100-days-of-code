8a1fab572e85fdc48c4a67cd8dd5bd1588a125ae
Colin <41882674+TheLabyrinthMind101@users.noreply.github.com>
2026-06-11 22:56:22 -0400

    Add files via upload

--- projects.js
+++ projects.js
@@ -36,25 +36,34 @@ print("Built by Colin Jacobs — 100 Days of Code")`
 
 import math
 number_to_check = int(input("Enter the number"))
-dividers = []
-num_sqrt = int(math.sqrt(number_to_check))
-if number_to_check < 2:
-    print("Numbers less than 2 are not prime.")
-elif number_to_check == 2:
-    print("Number is prime")
-#elif number_to_check % 2 == 0:
-#    print("Number is even and therefore not prime")
-else:
-    for number in range(2,num_sqrt+1):
-        if number_to_check % number == 0:
-            dividers.append(number)
-            dividers.append(int(number_to_check/number))
-    if not dividers:
-        print(f"{number_to_check} is prime.")
+again = 'not done'
+while again != 'done':
+    number_to_check = int(number_to_check)
+    dividers = []
+    num_sqrt = int(math.sqrt(number_to_check))
+    if number_to_check < 2:
+        print("Numbers less than 2 are not prime.")
+    elif number_to_check == 2:
+        print("Number is prime")
+    #elif number_to_check % 2 == 0:
+    #    print("Number is even and therefore not prime")
     else:
-        unique_dividers = list(dict.fromkeys(dividers))
-        print(f"{number_to_check} is not prime.")
-        print(f"Dividers: {sorted(unique_dividers)}")`
+        for number in range(2,num_sqrt+1):
+            if number_to_check % number == 0:
+                dividers.append(number)
+                dividers.append(int(number_to_check/number))
+        if not dividers:
+            print(f"{number_to_check} is prime.")
+        else:
+            unique_dividers = list(dict.fromkeys(dividers))
+            print(f"{number_to_check} is not prime.")
+            print(f"Dividers: {sorted(unique_dividers)}")
+    response = input("Check another number or type 'done' if you are finished.")
+    if response != 'done':
+        number_to_check = int(response)
+    else:
+        print("Goodbye!")
+        break`
   },
 
 
